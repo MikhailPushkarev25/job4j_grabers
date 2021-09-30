428ab9491fa2d59458140bd47e8be798d23eee61
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-09-30 22:00:58 +0300

    1. Quartz [#175122 #14947]

---
+++ src/main/java/ru/job4j/quartzAlertRabbit.java
@@ -0,0 +1,53 @@
+package ru.job4j;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class quartzAlertRabbit {
+    public static void main(String[] args) throws IOException {
+        Properties properties = getProperties();
+        String prop = properties.getProperty("rabbit.interval");
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(prop))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        try (BufferedReader in = new BufferedReader(
+                new FileReader("./src/main/resources/rabbit.properties"))) {
+            properties.load(in);
+        }
+        return properties;
+    }
+}
